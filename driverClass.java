50ffd6d8ae8db99a3117b30615aaf0720baffa10
Sheral <yoko7d11@outlook.com>
2020-05-06 20:19:04 +0100

    Add files via upload

---
+++ driverClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+import java.io.*;
+
+
+import java.sql.SQLException;
+/**
+ *
+ * @author sheral-annthompson
+ */
+public class driverClass /***implements TrackingServiceInterface*/ {
+//    static String trackingNumber = "";
+//
+//    @Override
+//    public void generateTrackingNumber() {
+//             for (int i = 0; i < 16; i++) {
+//            trackingNumber += charSet.charAt(random.nextInt(charSetLength));
+//        }  
+//    }
+
+public static void main(String[] args) throws SQLException {
+//    
+//    Users customer = new Users(7, trackingNumber, "sheral@hotmail.com","Chemical", "M14 3DT");
+//    customer.printUser();
+
+}
+}
